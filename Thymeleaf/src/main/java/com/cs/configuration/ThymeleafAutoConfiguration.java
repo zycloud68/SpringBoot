e6d1ee32f623eac27bba36b573b55058a4791f84
zycloud68 <yifan305>
2021-08-03 10:55:42 +0800

    Created Thymeleaf

--- Thymeleaf/src/main/java/com/cs/configuration/ThymeleafAutoConfiguration.java
+++ Thymeleaf/src/main/java/com/cs/configuration/ThymeleafAutoConfiguration.java
@@ -1,11 +1,9 @@
 package com.cs.configuration;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 
-@ConfigurationProperties(prefix = "spring.thymeleaf")
 public class ThymeleafAutoConfiguration {
     private static  final Charset DeFAULT_ENCODING = StandardCharsets.UTF_8;
 
